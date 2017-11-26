40ad18fecf4ff5fca13675028017253e7a664a76
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2017-11-25 23:45:47 -0600

    Stack

---
+++ Stack.py
@@ -0,0 +1,25 @@
+from sys import maxsize
+
+def createStack():
+    Stack = []
+    return Stack
+
+def isEmpty(Stack):
+    return len(Stack)==0
+
+def push(Stack, item):
+    Stack.append(item)
+    print("pushed to stack {0}".format(item))
+
+
+def pop(Stack):
+    if isEmpty(Stack):
+        print("Empty Stack cant be poped")
+    else:
+        return print(Stack.pop())
+
+stk = createStack()
+push(stk, 10)
+push(stk, 20)
+pop(stk)
+
