56f944c0e5cc270f63786c9a896cb72a7e522011
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-01-02 23:55:49 -0800

    Deque - Class Implementation

--- DEQUE/deque.py
+++ DEQUE/deque.py
@@ -6,16 +6,27 @@ class deque:
         return self.items == []
 
     def addFront(self, item):
-        self.items.insert(item)
+        self.items.append(item)
 
     def addRear(self,item):
         self.items.insert(0,item)
 
     def removeFront(self):
-        self.items.pop()
+        return self.items.pop()
 
     def removeRear(self):
-        self.items.pop(0)
+        return self.items.pop(0)
 
     def size(self):
         return len(self.items)
+
+d=deque()
+d.addRear(4)
+d.addRear('dog')
+d.addFront('cat')
+d.addFront(True)
+print(d.size())
+print(d.isEmpty())
+d.addRear(8.4)
+print(d.removeRear())
+print(d.removeFront())
