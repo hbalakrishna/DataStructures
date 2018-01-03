bd84f5aa3cbea73388daf59717eec7f7007df7f1
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-01-03 00:04:46 -0800

    Deque - Pallindrome Implementation

--- DEQUE/pallindrome.py
+++ DEQUE/pallindrome.py
@@ -1,1 +1,21 @@
 from DEQUE.deque import deque
+
+def palchecker(inString):
+    chardeque = deque()
+
+    for i in inString:
+        chardeque.addRear(i)
+
+    checkEqual = True
+
+    while chardeque.size()>1 and checkEqual:
+        firstChar = chardeque.removeRear()
+        lastChar = chardeque.removeFront()
+
+        if firstChar!=lastChar:
+            checkEqual = False
+
+    return checkEqual
+
+print(palchecker("radar"))
+print(palchecker("afhjshjfshbba"))
