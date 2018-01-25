8b807ae006447569fcd363f2f0bc18a2247382db
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-01-25 13:36:12 -0600

    Recursion - Recurssion-2

---
+++ rec3.py
@@ -0,0 +1,24 @@
+a = [2,3,4,4,4]
+
+# # if 2 in a:
+# #     a.remove(a.index(2))
+# val = 4
+#
+# b = a
+#
+# b = [i for i in a if i!= val]
+#
+# print(b)
+
+
+def removeElement(nums, val):
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+    b = [i for i in nums if i != val]
+    return b
+
+
+print(removeElement(a, 4))
