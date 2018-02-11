3e01e4930e75721a7310cadae1135c730df93c85
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-02-10 20:28:42 -0600

    set minus set

---
+++ Code/present.py
@@ -0,0 +1,14 @@
+a = [2,2]
+min_ = 1
+max_ = len(a)
+b = []
+
+if not b:
+    print('Empty')
+
+# Time limit exceeded
+res = [i for i in range(1, max_ + 1) if i not in a]
+print(res)
+
+#set minus set
+print(list(set(range(1, max_ + 1)) - set(a)))
