ba2c5100b4d84b5b29087335bd1160ea290108a7
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-02-12 23:17:14 -0600

    DP - Update

--- Code/DP.py
+++ Code/DP.py
@@ -11,3 +11,14 @@ def recMC(coinValueList,change):
    return minCoins
 
 print(recMC([1,5,10,25],63))
+
+
+#Update
+def dpMakeChange(coinValueList,change,minCoins):
+   for cents in range(change+1):
+      coinCount = cents
+      for j in [c for c in coinValueList if c <= cents]:
+            if minCoins[cents-j] + 1 < coinCount:
+               coinCount = minCoins[cents-j]+1
+      minCoins[cents] = coinCount
+   return minCoins[change]
