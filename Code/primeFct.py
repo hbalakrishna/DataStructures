e32436c039e73e637cb7f331f40816bdac638d23
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-02-15 19:54:13 -0600

    prime

---
+++ Code/primeFct.py
@@ -0,0 +1,24 @@
+def primes(n):
+    primfac = []
+    d = 2
+    while d*d <= n:
+        while (n % d) == 0:
+            primfac.append(d)  # supposing you want multiple factors repeated
+            n //= d
+        d += 1
+    if n > 1:
+       primfac.append(n)
+    return primfac
+
+ans = primes()
+chk = [1, 2, 3, 5]
+cnt = 0
+
+for i in ans:
+    if i in chk:
+        cnt += 1
+
+if cnt==len(ans):
+    print("Ugly")
+else:
+    print("not Ugly")
