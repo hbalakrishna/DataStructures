3c7f0de218af72664fad3ccfbaff9d909f554b2a
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-03-05 13:58:40 -0600

    self divide - unoptimized

--- Code/selfD.py
+++ Code/selfD.py
@@ -1,7 +1,18 @@
-x = 10
+x = 22
 ans = []
 
 for i in range(1, x + 1):
-    if len(str(i)) > 1:
-        
-        print(i, len(str(i)))
+    if len(str(i)) == 0:
+        ans.append(i)
+    else:
+        cnt_ = 0
+        for k in range(len(str(i))):
+            if int(str(i)[k]) == 0:
+                break
+            if i % int(str(i)[k]) == 0:
+                cnt_ += 1
+
+        if cnt_ == len(str(i)):
+            ans.append(i)
+
+print(ans)
