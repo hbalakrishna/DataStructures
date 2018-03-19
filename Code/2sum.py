8753f42e2e809e6abe3e3c5da25b1b858b644fdf
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-03-18 20:49:27 -0500

    sumof2

--- Code/2sum.py
+++ Code/2sum.py
@@ -1,14 +1,21 @@
-nums = [3,2,4]
-target = 6
+nums = [2, 7, 11, 15]
+target = 9
+d = {}
 
-ans = []
+for i, num in enumerate(nums):
+    if target - num in d:
+        print(d[target - num] + 1, i + 1)
+    d[num] = i
 
-for i in range(len(nums)):
-    target_ = target - nums[i]
-    nums[i] = "$"
-    if target_ in nums:
-        ans.append(i)
-        ans.append(nums.index(target_))
-        break
 
-print(ans)
+# for i in range(len(nums)):
+#     target_ = target - nums[i]
+#     nums[i] = "$"
+#     if target_ in nums:
+#         # ans.append(i + 1)
+#         idx1 = i + 1
+#         # ans.append(nums.index(target_) + 1)
+#         idx2 = nums.index(target_) + 1
+#         break
+
+# print(idx1, idx2)
