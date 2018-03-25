64b6adf30ffec8ffcbe46a80db5dd3c178a36a87
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2018-03-25 12:26:55 -0500

    string - KMP Algorithm

---
+++ String/KMP_SubString.py
@@ -0,0 +1,25 @@
+ret = [0]
+str_ = "abcaby"
+
+def patMatch(pattern):
+    for i in range(1, len(str_)):
+        j = ret[i - 1]
+        while j > 0 and str_[i]!=str_[j]:
+            j = ret[j-1]
+        ret.append(j + 1 if str_[i] == str_[j] else j)
+    return ret
+
+def subString():
+    string = "abcabxabcaby"
+    pattern, result,j = patMatch(str_), [],0
+
+    for i in range(len(string)):
+        while j>0 and string[i]!=str_[j]:
+            j = pattern[j-1]
+        if string[i]==str_[j]:
+            j += 1
+        if j == len(pattern):
+            result.append(i - (j - 1))
+    return result
+
+print(subString())
